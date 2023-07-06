53fd4495f4e6cba938c1fc2b8889c2a6c0c9a28f
higiniecito <cdramirezh@unal.edu.co>
2023-07-05 20:40:58 -0500

    Extendiendo el App component

---
+++ components/Layout/Layout.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Navbar from "components/Navbar/Navbar";
+
+type Props = {
+	children: React.ReactNode
+}
+
+const Layout: React.FC<Props> = ({ children }) => {
+	return (
+		<>
+			<Navbar />
+			{children}
+			<footer>This is the footer</footer>
+		</>
+	);
+};
+
+export default Layout;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react'
-import Navbar from '../components/Navbar/Navbar'
 
 const API = '/api/avo'
 
@@ -13,7 +12,6 @@ const HomePage = () => {
   }, [])
   return (
     <div>
-      <Navbar />
       <div>Platzi and Next.js!</div>
       {productList.map((producto) => (
         <div key={producto.id}>{producto.name}</div>
