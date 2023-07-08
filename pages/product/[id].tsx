a25ef47deadbc3af550799aac2b4cdb0cde077fc
higiniecito <cdramirezh@unal.edu.co>
2023-07-08 10:50:33 -0500

    Static Site Generation con páginas "dinámicas"

--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -1,23 +1,37 @@
-import React, { useState, useEffect } from "react";
-import { useRouter } from "next/router";
+import React from "react";
+import { GetStaticProps } from "next";
 
 import Layout from "@components/Layout/Layout";
 import ProductSummary from "@components/ProductSummary/ProductSummary";
 
-const ProductPage = () => {
-	const { query } = useRouter();
-	const [product, setProduct] = useState<TProduct | null>(null);
+// si usamos getStaticProps para una página dinámica,
+// entonces toca pasarle TODOS los posibles versiones de la página
+// la función getStaticPaths obtiene esos paths para que Next genere todas las pages
+export const getStaticPaths = async () => {
+	const response = await fetch("https://avocados-three.vercel.app/api/avo");
+	const { data: productList }: TAPIAvoResponse = await response.json();
 
-	useEffect(() => {
-		if (query.id) {
-			fetch(`/api/avo/${query.id}`)
-				.then((response) => response.json())
-				.then((data: TProduct) => {
-					setProduct(data);
-				});
-		}
-	}, [query.id]);
+	// los paths que devuelve la función deben tener la siguiente estructura
+	const paths = productList.map(({ id }) => ({ params: { id } }));
 
+	return {
+		paths,
+		// incremental static generation
+		// anything not specified in paths returns 404
+		fallback: false,
+	};
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+	const id = params?.id as string;
+	const response = await fetch(
+		`https://avocados-three.vercel.app/api/avo/${id}`
+	);
+	const product: TAPIAvoResponse = await response.json();
+	return { props: { product } };
+};
+
+const ProductPage = ({ product }: { product: TProduct }) => {
 	return (
 		<Layout>
 			{product == null ? null : <ProductSummary product={product} />}
